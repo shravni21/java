f986b5daf1aa3783fdd00aa44e058ae7f81d6575
unknown <wakdeshravni@gmail.com>
2024-02-21 17:51:31 +0530

    Single responsibility principle

--- OOP_Design/lesson_1/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-       vehicle mycar= new vehicle();
-       vehicle anotherCar= new vehicle();
-       mycar.accelerate();
-       anotherCar.accelerate();
-    }
-}
